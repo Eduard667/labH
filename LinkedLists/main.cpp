3c3292a965c1ccf7b1b303c08db3641c9b357fd7
Eduard Stefan <e.stefan-2023@hull.ac.uk>
2026-03-19 18:48:24 +0000

    updated main file

--- LinkedLists/main.cpp
+++ LinkedLists/main.cpp
@@ -4,12 +4,6 @@
 using namespace std;
 #include "AddressBookSLL.h";
 
-/*
-	This program will act as a Contacts Address Book using a Single Linked Lists
-*/
-
-
-
 int main(int argc, char **argv) {
 
 	AddressBookSLL book;
